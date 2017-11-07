d3c92c497ad6003c11f84272b3d7596c201f2918
Ricardo Silva <ricardo@loadingplay.com>
2017-11-07 14:54:43 -0300

    fix noticias

--- fcv/scripts/payment.js
+++ fcv/scripts/payment.js
@@ -1,36 +1,57 @@
 
 $(document).ready(function()
 {
+	$.cookie('shopping-cart', '');
+
 	var sendToCheckout = function(price)
 	{
 		var facade = $('.products').data('ecommerce');
 
 		// delete all other products
-		facade.ecommerce.cart.clearCart(function()
-		{
+		facade.ecommerce.cart.clearCart();
 
+		setTimeout(function() {
 			// add the current product
+			// id, sku, combination, price, name, upp, bullet1, bullet2, bullet3, img, callback
 			facade.ecommerce.cart.addProduct(
 				5688, // product.id, 
+				"DONACION",
+				"",
 				price,// product.price, 
 				"donacion", // product.name, 
 				1, // product.upp, 
 				"", // product.bullet1, 
 				"", // product.bullet2, 
 				"", // product.bullet3,
-				"", // product.images, 
-				function()
-				{
-					// proceed to checkout
-					var checkout = facade.ecommerce.cart.view.getCheckoutData();
-					facade.ecommerce.cart.view.goToCheckout(checkout);
-				});
-		});
+				"" // product.images, 
+			);
+
+			setTimeout(function() {
+				// proceed to checkout
+				// var checkout = facade.ecommerce.cart.view.getCheckoutData();
+				// $("#checkout-iframe").attr('src', facade.ecommerce.cart.getCheckoutUrl() + 
+				// 	'?' +
+				// 	'site_id=' + facade.ecommerce.cart.getSiteId() +
+				// 	'&cart_id=' + facade.ecommerce.cart.getGUID());
+
+				// $('#myModal').modal('show');
+				// var a = $('<a href="'+ facade.ecommerce.cart.getCheckoutUrl() + 
+				// 	'?' +
+				// 	'site_id=' + facade.ecommerce.cart.getSiteId() +
+				// 	'&cart_id=' + facade.ecommerce.cart.getGUID() + '" target="_blank" ></a>')
+				// a.click();
+				document.location.href = facade.ecommerce.cart.getCheckoutUrl() + 
+					'?' +
+					'site_id=' + facade.ecommerce.cart.getSiteId() +
+					'&cart_id=' + facade.ecommerce.cart.getGUID();
+			}, 1000);
+		}, 1000);
 	};
 
 	var ecommerce = $('.products').ecommerce({
-		app_public: 40
-		// base_url: 'https://betaapi.loadingplay.com'
+		app_public: 40,
+		base_url: 'https://betaapi.loadingplay.com',
+		checkout_url: 'https://betapay.loadingplay.com'
 	});
 
 	$('select[name=select-donacion]').on('change', function()
@@ -52,34 +73,34 @@ $(document).ready(function()
 	});
 	$(".single-donation-button").click(function(e)
 	{
-	  e.preventDefault();
-	  var selected = $("input[name=radio-ammount]:checked").val();
-	  var ammouunt = 0;
+		e.preventDefault();
+		var selected = $("input[name=radio-ammount]:checked").val();
+		var ammouunt = 0;
 
-	  if (selected === "otro")
-	  {
-		var val = parseInt($("input[name=ammout]").val());
-		// check if not a number
-		if (isNaN(val))
+		if (selected === "otro")
 		{
-			$("input[name=ammout]").css('border', 'red solid 1px');
-			return false;
+			var val = parseInt($("input[name=ammout]").val());
+			// check if not a number
+			if (isNaN(val))
+			{
+				$("input[name=ammout]").css('border', 'red solid 1px');
+				return false;
+			}
+			else
+			{
+				$("input[name=ammout]").css('border', 'black solid 1px');
+			}
+
+			selected = "" + val;
+
 		}
-		else
-		{
-		  $("input[name=ammout]").css('border', 'black solid 1px');
-		}
 
-		selected = "" + val;
+		// clean
+		selected = selected.split('.').join('');
+		selected = selected.split('$').join('');
 
-	  }
-
-	  // clean
-	  selected = selected.split('.').join('');
-	  selected = selected.split('$').join('');
-
-	  $(this).attr('disabled', 'disabled');
-	  sendToCheckout(selected);
+		$(this).attr('disabled', 'disabled');
+		sendToCheckout(selected);
 
 	});
 });
