c8b5a674f2a8b3230b66dcfa4445502cb4ef06e0
Ricardo Silva <ricardo@loadingplay.com>
2017-11-17 20:01:42 -0300

    added loadingplay

---
+++ loadingplay/static/cssbt/style.css
@@ -0,0 +1,10 @@
+Traceback (most recent call last):
+  File "/var/www/sites.back/venv/local/lib/python2.7/site-packages/tornado/web.py", line 1338, in _execute
+    self.finish()
+  File "/var/www/sites.back/venv/local/lib/python2.7/site-packages/tornado/web.py", line 877, in finish
+    self.set_etag_header()
+  File "/var/www/sites.back/venv/local/lib/python2.7/site-packages/tornado/web.py", line 1257, in set_etag_header
+    etag = self.compute_etag()
+  File "/var/www/sites.back/venv/local/lib/python2.7/site-packages/tornado/web.py", line 2185, in compute_etag
+    version_hash = self._get_cached_version(self.absolute_path)
+AttributeError: 'SiteStaticHandler' object has no attribute 'absolute_path'
