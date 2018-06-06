e1c9411401df6ed8d64ae0fc30bdb55b1f248f96
Ricardo Silva <ricardo@loadingplay.com>
2018-06-06 11:04:21 -0400

    working version

--- fcv/scripts/payment.js
+++ fcv/scripts/payment.js
@@ -1,93 +1,93 @@
 
 $(document).ready(function()
 {
-	$.cookie('shopping-cart', '');
+    $.cookie('shopping-cart', '');
 
-	var sendToCheckout = function(price)
-	{
-		var facade = $('.products').data('ecommerce');
+    var sendToCheckout = function(price)
+    {
 
-		// delete all other products
-		// facade.ecommerce.cart.clearCart();
+        var facade = $('.products').data('ecommerce');
 
-		// add the current product
-		// id, sku, combination, price, name, upp, bullet1, bullet2, bullet3, img, callback
-		facade.ecommerce.cart.addProduct(
-			5688, // product.id,
-			"DONACION",
-			"",
-			price,// product.price,
-			"donacion", // product.name,
-			1, // product.upp,
-			"", // product.bullet1,
-			"", // product.bullet2,
-			"", // product.bullet3,
-			"", // product.images,
-			function()
-			{
-				setTimeout(function() {
-					document.location.href = facade.ecommerce.cart.getCheckoutUrl() +
-						'?' +
-						'site_id=' + facade.ecommerce.cart.getSiteId() +
-						'&cart_id=' + facade.ecommerce.cart.getGUID();
-				}, 1000);
-			}
-		);
-	};
+        // delete all other products
+        // facade.ecommerce.cart.clearCart();
 
-	var ecommerce = $('.products').ecommerce({
-		app_public: 40,
-		base_url: 'https://apibodegas.loadingplay.com',
-		checkout_url: 'https://betapay.loadingplay.com'
-	});
+        // add the current product
+        // id, sku, combination, price, name, upp, bullet1, bullet2, bullet3, img, callback
+        facade.ecommerce.cart.addProduct(
+            5688, // product.id,
+            "DONACION",
+            "",
+            price,// product.price,
+            "donacion", // product.name,
+            1, // product.upp,
+            "", // product.bullet1,
+            "", // product.bullet2,
+            "", // product.bullet3,
+            "" // product.images,
+        );
 
-	$('select[name=select-donacion]').on('change', function()
-	{
-		console.log($(this).val());
-		var val = $(this).val();
+        setTimeout(function() {
+            document.location.href = facade.ecommerce.cart.getCheckoutUrl() +
+                '?' +
+                'site_id=' + facade.ecommerce.cart.getSiteId() +
+                '&cart_id=' + facade.ecommerce.cart.getGUID();
+        }, 2000);
+    };
 
-		if (val === 'otro')
-		{
-			// show textbox
-			$("input[name=donacion]").attr("type", "text");
-			$("input[name=donacion]").focus();
-		}
-		else
-		{
-			$("input[name=donacion]").attr("type", "hidden");
-			$("input[name=donacion]").val(val);
-		}
-	});
-	$(".single-donation-button").click(function(e)
-	{
-		e.preventDefault();
-		var selected = $("input[name=radio-ammount]:checked").val();
-		var ammouunt = 0;
+    var ecommerce = $('.products').ecommerce({
+        app_public: 40,
+        base_url: 'https://apibodegas.loadingplay.com',
+        checkout_url: 'https://pay.loadingplay.com'
+    });
 
-		if (selected === "otro")
-		{
-			var val = parseInt($("input[name=ammout]").val());
-			// check if not a number
-			if (isNaN(val))
-			{
-				$("input[name=ammout]").css('border', 'red solid 1px');
-				return false;
-			}
-			else
-			{
-				$("input[name=ammout]").css('border', 'black solid 1px');
-			}
+    $('select[name=select-donacion]').on('change', function()
+    {
+        console.log($(this).val());
+        var val = $(this).val();
 
-			selected = "" + val;
+        if (val === 'otro')
+        {
+            // show textbox
+            $("input[name=donacion]").attr("type", "text");
+            $("input[name=donacion]").focus();
+        }
+        else
+        {
+            $("input[name=donacion]").attr("type", "hidden");
+            $("input[name=donacion]").val(val);
+        }
+    });
+    $(".single-donation-button").click(function(e)
+    {
+        e.preventDefault();
+        var selected = $("input[name=radio-ammount]:checked").val();
+        var ammouunt = 0;
 
-		}
+        if (selected === "otro")
+        {
+            var val = parseInt($("input[name=ammout]").val());
+            // check if not a number
+            if (isNaN(val))
+            {
+                $("input[name=ammout]").css('border', 'red solid 1px');
+                return false;
+            }
+            else
+            {
+                $("input[name=ammout]").css('border', 'black solid 1px');
+            }
 
-		// clean
-		selected = selected.split('.').join('');
-		selected = selected.split('$').join('');
+            selected = "" + val;
 
-		$(this).attr('disabled', 'disabled');
-		sendToCheckout(selected);
+        }
 
-	});
+        // clean
+        selected = selected.split('.').join('');
+        selected = selected.split('$').join('');
+
+        $(this).attr('disabled', 'disabled');
+        $(this).html('cargando...');
+        sendToCheckout(selected);
+
+    });
 });
